0bd9cf2e6d5f7b70d6a9c2418f0378aff6f3c527
RonyVidaur <rony.vidaur@gmail.com>
2017-01-11 19:17:44 -0600

    updated dependencies

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ var bodyParser = require('body-parser')
 var path = require('path')
 /*initializing the variable for the server*/
 var app = express()
+/*
+var logger = function(req, res, next) {
+    console.log('logging...')
+    next()
+}
 
+app.use(logger)
+*/
+app.use(bodyParser.json())
+app.use(bodyParser.url())
 app.get('/', function(req, res){
     res.send('Hello World')
 })
